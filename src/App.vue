b2d0dd2a34a60799902f52d0ce72a8a6d403d20e
danielcrubens <dcr.tecnologi@gmail.com>
2023-05-01 18:03:38 -0300

    estruturando  projeto :memo:

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <h1 class="text-3xl bg-red-600 font-bold underline">
+    Hello world!
+  </h1>
+</template>
